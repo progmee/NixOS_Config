0392f63a2e976fc65036aff45d2c3b768787da21
progmee <demidovalex2005@gmail.com>
2026-05-07 00:30:53 +0200

    Add lazygit to console

--- programs/shell.nix
+++ programs/shell.nix
@@ -31,9 +31,13 @@
   environment.shellAliases = {
     # Zoxide replacement
     cd = "z";
+
+    lg = "lazygit";
+    
     # System monitoring aliases
     htop = "btop";
     top = "btop";
+
     # Neovim aliases
     v = "nvim";
     vi = "nvim";
@@ -42,6 +46,7 @@
 
     # Fastfetch aliases (using 'ff' to avoid recursion)
     fetch = "fastfetch --logo none --structure OS:Kernel:Uptime:Packages:Shell:Display:CPU:GPU:Memory:Disk:Break";
+    
     # Batman aliases
     man = "batman";
 
@@ -56,9 +61,11 @@
     la = "eza -lbhHigUmuSa --time-style=long-iso --git --color-scale --icons";
     lt = "eza --tree --level=2 --icons";
     tree = "eza --tree --icons";
+
     # Search and find aliases
     grep = "rg";
     find = "fd";
+
     # System update shortcut
     rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#nixos";
     clean = "clear && sudo nix-collect-garbage -d && sudo nix-store --optimize";
