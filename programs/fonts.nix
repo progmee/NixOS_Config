74fe31b21971048a9445ee9f6d4becdaf948cc74
progmee <demidovalex2005@gmail.com>
2026-05-08 15:26:42 +0200

    Update fonts list

--- programs/fonts.nix
+++ programs/fonts.nix
@@ -7,9 +7,6 @@
   fonts.packages = with pkgs; [ 
     # Main Nerd Fonts for icons and terminal
     nerd-fonts.jetbrains-mono
-    nerd-fonts.fira-code
-    nerd-fonts.meslo-lg
-    nerd-fonts.hack
     nerd-fonts.symbols-only # Just icons, very useful for Waybar
   ];
 }
