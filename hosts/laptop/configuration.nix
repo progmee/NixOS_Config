7dd3d0b475833cdec0cfcae331619630853d6494
Aleksandr <progme.contact@gmail.com>
2026-08-26 20:28:22 +0200

    Adapted update command for NixOS and created .gitignore

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -15,7 +15,7 @@
   ];
 
   # Define the network hostname for the system
-  networking.hostName = "laptop";
+  networking.hostName = "progme-laptop";
 
   # Define the system state version (do not change after initial installation)
   system.stateVersion = "26.05";
--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -6,15 +6,17 @@
     ../../modules/core/users.nix
     ../../modules/core/locale.nix
     ../../modules/core/nix.nix
+    ../../modules/services/open-ssh.nix
   ];
 
   wsl = {
     enable = true;
     defaultUser = "progme";
   };
+  wsl.interop.includePath = false;
 
   # Define the network hostname for the system
-  networking.hostName = "nixos-wsl";
+  networking.hostName = "progme-wsl";
 
   # Define the system state version (do not change after initial installation)
   system.stateVersion = "26.05";
