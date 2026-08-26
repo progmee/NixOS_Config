d9bea874bb351698ed1a5657e839fadd635bec44
Aleksandr <demidovalex2005@gmail.com>
2026-08-26 08:11:36 +0200

    Created new wsl flakes profile and boot options changed to GRUB

---
+++ hosts/wsl/configuration.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    # Include hardware scan results
+    ./hardware-configuration.nix
+
+    # Include core system modules (automatically loads default.nix from core folder)
+    ../../modules/core
+
+    # Include multimedia modules
+    ../../modules/desktop/sound.nix
+  ];
+
+  # Define the network hostname for the system
+  networking.hostName = "laptop";
+
+  # Define the system state version (do not change after initial installation)
+  system.stateVersion = "26.05";
+}
--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,7 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  # Bootloader configuration: using systemd-boot with EFI support
-  boot.loader.systemd-boot.enable = true;
+  # Bootloader configuration: using GRUB with EFI support
+  boot.loader.systemd-boot.enable = false;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    device = "nodev"; # Раскомментируйте, если используется UEFI (обычно требуется для EFI)
+  };
 }
