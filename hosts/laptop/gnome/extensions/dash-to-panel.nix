d8f61cfd5b8aaab8f1eb92901b17c9563fd4a66c
Aleksandr <demidovalex2005@gmail.com>
2026-08-29 20:14:03 +0200

    Fix dash to panel extensions params

--- hosts/laptop/gnome/extensions/dash-to-panel.nix
+++ hosts/laptop/gnome/extensions/dash-to-panel.nix
@@ -20,11 +20,15 @@
       hotkeys-overlay-combo = "TEMPORARILY";
       leftbox-padding = 4;
       leftbox-size = 16;
-      panel-anchors = ''{"unknown-unknown":"MIDDLE"}'';
-      panel-element-positions = ''{"unknown-unknown":[{"element":"showAppsButton","visible":false,"position":"stackedTL"},{"element":"activitiesButton","visible":false,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"centerMonitor"},{"element":"taskbar","visible":true,"position":"centerMonitor"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"dateMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":true,"position":"stackedBR"}]}'';
-      panel-lengths = "{}";
-      panel-positions = "{}";
-      panel-sizes = "{}";
+      
+      # Use monitor index '0' instead of unknown-unknown for reliable application
+      panel-anchors = ''{"0":"MIDDLE"}'';
+      panel-element-positions = ''{"0":[{"element":"showAppsButton","visible":false,"position":"stackedTL"},{"element":"activitiesButton","visible":false,"position":"stackedTL"},{"element":"leftBox","visible":true,"position":"centerMonitor"},{"element":"taskbar","visible":true,"position":"centerMonitor"},{"element":"centerBox","visible":true,"position":"stackedBR"},{"element":"rightBox","visible":true,"position":"stackedBR"},{"element":"systemMenu","visible":true,"position":"stackedBR"},{"element":"dateMenu","visible":true,"position":"stackedBR"},{"element":"desktopButton","visible":true,"position":"stackedBR"}]}'';
+      
+      panel-lengths = ''{"0":100}'';
+      panel-positions = ''{"0":"BOTTOM"}''; # или оставьте BOTTOM в зависимости от того, куда хотите закрепить
+      panel-sizes = ''{"0":48}'';
+      
       prefs-opened = false;
       status-icon-padding = 4;
       tray-padding = 4;
